136487eb2732afb244b28cc8aba988f0ae02756d
imswagk727 <imswagk727@gmail.com>
2023-10-17 22:18:16 +0800

    1.2 dont (wait for testing)

--- src/app/pages/page1/page1.component.ts
+++ src/app/pages/page1/page1.component.ts
@@ -4,7 +4,7 @@ import { CsvDataService } from 'src/app/shared/service/csv-data.service';
 @Component({
   selector: 'app-page1',
   templateUrl: './page1.component.html',
-  styleUrls: ['./page1.component.css']
+  styleUrls: ['./page1.component.css'],
 })
 export class Page1Component implements OnInit {
   csvData: { headers: string[]; rows: string[][] } | null = null;
@@ -19,17 +19,19 @@ export class Page1Component implements OnInit {
   listOfOperatorNumeical: string[] = ['>', '<', '=', '!=']; // filter operator
   listOfOperatorCategorical: string[] = ['=', '!='];
 
-  filterGroups: {  //
+  filterGroups: {
+    //
     listOfSelectedFilterColumn: string; // selected column
     listOfSelectedOperator: string; // selected operator
     listOfSelectedFilterValue: any[]; // selected value
     isNumber: boolean;
     isDiverseNumber: boolean;
+    minValue: number | null;
+    maxValue: number | null;
   }[] = [];
-  
+
   // isNumber: boolean = false;
   // isDiverseNumber: boolean =false;
-
 
   constructor(private csvDataService: CsvDataService) {
     console.log('CsvDataService injected');
@@ -43,12 +45,16 @@ export class Page1Component implements OnInit {
     if (this.csvData && this.csvData.headers) {
       this.listOfOption = this.csvData.headers;
     }
-
   }
 
   // Reset the target variable if it matches any selected feature variable
   resetTargetIfMatch() {
-    if (this.listOfselectedTargetVariable && this.listOfSelectedFeatureVariables.includes(this.listOfselectedTargetVariable)) {
+    if (
+      this.listOfselectedTargetVariable &&
+      this.listOfSelectedFeatureVariables.includes(
+        this.listOfselectedTargetVariable
+      )
+    ) {
       this.listOfselectedTargetVariable = null;
     }
   }
@@ -65,11 +71,13 @@ export class Page1Component implements OnInit {
   // add new filter group
   addFilterGroup() {
     this.filterGroups.push({
-      listOfSelectedFilterColumn: '', 
+      listOfSelectedFilterColumn: '',
       listOfSelectedOperator: '',
       listOfSelectedFilterValue: [],
       isNumber: false,
-      isDiverseNumber: false
+      isDiverseNumber: false,
+      minValue: null,
+      maxValue: null,
     });
   }
 
@@ -83,42 +91,58 @@ export class Page1Component implements OnInit {
     const group = this.filterGroups[index];
     // 获取当前所选列名
     const selectedColumn = group.listOfSelectedFilterColumn;
-  
+
     // 初始化结果数组
     const selectedValues: any[] = [];
-  
+
     // 检查所选列名是否为空
     if (selectedColumn && typeof selectedColumn === 'string') {
       // 在数据源中查找与所选列名匹配的数据行
       if (this.csvData && this.csvData.headers && this.csvData.rows) {
         const columnIndex = this.csvData.headers.indexOf(selectedColumn);
-  
+
         if (columnIndex !== -1) {
           // 使用数组映射（map）来获取所选列的所有数据行
-          this.csvData.rows.forEach(row => {
+          this.csvData.rows.forEach((row) => {
             selectedValues.push(row[columnIndex]);
           });
         }
       }
     }
-  
+
     group.listOfSelectedFilterValue = selectedValues;
-    console.log('data column:', group.listOfSelectedFilterColumn)
-    console.log('data value:',  group.listOfSelectedFilterValue)
-    group.isNumber = this.isNumeric(group.listOfSelectedFilterValue)
-    group.isDiverseNumber = this.hasMoreThanFiveUniqueNumbers(group.listOfSelectedFilterValue)
-    console.log('isNumber:', group.isNumber)
-    console.log('isDiverseNumber:', group.isDiverseNumber)
+    console.log('data column:', group.listOfSelectedFilterColumn);
+    console.log('data value:', group.listOfSelectedFilterValue);
+    group.isNumber = this.isNumeric(group.listOfSelectedFilterValue);
+    group.isDiverseNumber = this.hasMoreThanFiveUniqueNumbers(
+      group.listOfSelectedFilterValue
+    );
+    console.log('isNumber:', group.isNumber);
+    console.log('isDiverseNumber:', group.isDiverseNumber);
+
+    // Calculate min and max for numeric values
+    const numericValues = selectedValues
+      .map((value) => parseFloat(value))
+      .filter((value) => !isNaN(value));
+
+    if (numericValues.length > 0) {
+      group.minValue = Math.min(...numericValues);
+      group.maxValue = Math.max(...numericValues);
+    } else {
+      group.minValue = null;
+      group.maxValue = null;
+    }
+    console.log('Min:', group.minValue);
+    console.log('Max:', group.maxValue);
   }
 
-
-  isNumeric(columnData: string[]) :boolean{
-    return columnData.every(value => !isNaN(parseFloat(value)));
+  isNumeric(columnData: string[]): boolean {
+    return columnData.every((value) => !isNaN(parseFloat(value)));
   }
-  
-  hasMoreThanFiveUniqueNumbers(columnData: string[]) :boolean {
+
+  hasMoreThanFiveUniqueNumbers(columnData: string[]): boolean {
     const uniqueNumbers = new Set();
-  
+
     for (const value of columnData) {
       if (!isNaN(parseFloat(value))) {
         uniqueNumbers.add(parseFloat(value));
